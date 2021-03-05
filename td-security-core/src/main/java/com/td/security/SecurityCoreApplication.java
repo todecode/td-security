6cec7dff901719b6185344c5b8b3fab5cb49be4f
cyd <15016414752@163.com>
2021-03-05 10:55:18 +0800

    2-项目初始化

---
+++ td-security-core/src/main/java/com/td/security/SecurityCoreApplication.java
@@ -0,0 +1,13 @@
+package com.td.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityCoreApplication.class, args);
+    }
+
+}
---
+++ td-security-demo/src/main/java/com/td/security/SecurityDemoApplication.java
@@ -0,0 +1,21 @@
+package com.td.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SecurityDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityDemoApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
