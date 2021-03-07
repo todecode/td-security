8bb56c9c5ce94bef7c3d223e6f0f2da949e40d6f
cyd <15016414752@163.com>
2021-03-07 23:12:54 +0800

    包层级切换

--- td-security-browser/src/main/java/com/td/SecurityBrowserApplication.java
+++ td-security-browser/src/main/java/com/td/SecurityBrowserApplication.java
@@ -1,4 +1,4 @@
-package com.td.security;
+package com.td;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- td-security-demo/src/main/java/com/td/SecurityDemoApplication.java
+++ td-security-demo/src/main/java/com/td/SecurityDemoApplication.java
@@ -1,8 +1,7 @@
-package com.td.security;
+package com.td;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
