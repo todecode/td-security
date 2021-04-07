c2b7559eee5ec5b1aab7d920db08a12fd8d33e7a
cyd <15016414752@163.com>
2021-04-07 23:06:59 +0800

    4-2 SpringSecurity基本原理(目前formLogin依旧弹框，且登陆时候UsernamePasswordAuthenticationFilter没进去断点)

---
+++ td-security-browser/src/main/java/com/td/security/browser/BrowserSecurityConfig.java
@@ -0,0 +1,36 @@
+package com.td.security.browser;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @className: BrowserSecurityConfig
+ * @description: 浏览器安全配置
+ * @author: cyd
+ * @date: 2019/12/7 下午4:42
+ **/
+@Configuration
+public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter  {
+
+    /**
+     * formLogin() --> 表单登陆
+     * httpBasic() --> 最初弹框登陆
+     * authorizeRequests() -->对请求做一个授权
+     * anyRequest() --> 任何请求
+     * authenticated() --> 都需要身份证认证
+     * antMatchers（） --> 访问这个url permitAll 不需要身份认证
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // security默认配置的代码
+        http.formLogin()
+                .and()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated();
+
+    }
+}
